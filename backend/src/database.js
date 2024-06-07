efae4bd271ca930714163c8456b39e821351d22b
Valentin REJAUNIER <72859624+ValentinR01@users.noreply.github.com>
2024-06-08 00:41:48 +0200

    Update database.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,11 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASS:", typeof process.env.DB_PASS);
-console.log("DB_HOST:", process.env.DB_HOST);
-
 const sequelize = new Sequelize(
   process.env.DB_NAME, 
   process.env.DB_USER, 
